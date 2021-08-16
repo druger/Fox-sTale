c1878951393016b6881936e4039c07b9d0c024d9
Evgeny Gromov <bayt32@yandex.ru>
2021-08-16 15:47:31 +0300

    Setup camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform farBg;
+    [SerializeField] private Transform middleBg;
+    [SerializeField] private float minHeight;
+    [SerializeField] private float maxHeight;
+
+    private Vector2 _lastPos;
+
+    private void Start() {
+        _lastPos = transform.position;
+    }
+
+    void Update() {
+        transform.position = new Vector3(
+            target.position.x,
+            Mathf.Clamp(target.position.y, minHeight, maxHeight),
+            transform.position.z);
+
+        Vector2 amountToMove = new Vector2(
+            transform.position.x - _lastPos.x,
+            transform.position.y - _lastPos.y);
+
+        farBg.position = farBg.position + new Vector3(amountToMove.x, amountToMove.y);
+        middleBg.position += new Vector3(amountToMove.x, amountToMove.y) * .5f;
+
+        _lastPos = transform.position;
+    }
+}
