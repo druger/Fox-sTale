43786fc0bef0664f63595e8ca803ff9412398029
Evgeny Gromov <bayt32@yandex.ru>
2021-09-02 16:33:31 +0300

    Add Continue game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    [SerializeField] private GameObject continueBtn;
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("Level1_unlocked")) {
+            continueBtn.SetActive(true);
+        } else {
+            continueBtn.SetActive(false);
+        }
+    }
 
     public void StartGame() {
+        PlayerPrefs.DeleteAll();
         SceneManager.LoadScene(2);
+    }
+
+    public void ContinueGame() {
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame() {
