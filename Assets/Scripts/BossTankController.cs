73b9cca1e4d02f5b69f1762738cffc70a2a87e9a
Evgeny Gromov <bayt32@yandex.ru>
2021-09-09 16:30:39 +0300

    setup boss state

--- Assets/Scripts/BossTankController.cs
+++ Assets/Scripts/BossTankController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossTankController : MonoBehaviour {
-
     [SerializeField] private Transform boss;
     [SerializeField] private Animator animator;
+    [SerializeField] private States currentState;
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
@@ -24,8 +24,56 @@ public class BossTankController : MonoBehaviour {
     private float _hurtCounter;
 
     void Start() {
+        currentState = States.Shooting;
     }
 
     void Update() {
+        switch (currentState) {
+            case States.Shooting:
+                break;
+            case States.Hurt:
+                if (_hurtCounter > 0) {
+                    _hurtCounter -= Time.deltaTime;
+
+                    if (_hurtCounter <= 0) currentState = States.Moving;
+                }
+
+                break;
+            case States.Moving:
+                if (_moveRight) {
+                    boss.position += new Vector3(moveSpeed * Time.deltaTime, 0f);
+                    if (boss.position.x > rightPoint.position.x) {
+                        _moveRight = false;
+                        boss.localScale = Vector3.one;
+                        EndMovement();
+                    }
+                } else {
+                    boss.position -= new Vector3(moveSpeed * Time.deltaTime, 0f);
+                    if (boss.position.x < leftPoint.position.x) {
+                        _moveRight = true;
+                        boss.localScale = new Vector3(-1f, 1f, 1f);
+                        EndMovement();
+                    }
+                }
+                break;
+        }
+    }
+
+    private void EndMovement() {
+        currentState = States.Shooting;
+        _shotCounter = timeBetweenShots;
+        animator.SetTrigger("stopMoving");
+    }
+
+    private void TakeHit() {
+        currentState = States.Hurt;
+        _hurtCounter = hurtTime;
+        animator.SetTrigger("hit");
+    }
+
+    private enum States {
+        Shooting,
+        Hurt,
+        Moving
     }
 }
